fab0a95963f7686cadb735f43c1464d1a7141a6e
Onowoni <danielonowoni1@gmail.com>
2022-08-23 13:03:49 +0100

    styling

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app.utils import save_picture, save_profile_picture
 @app.route('/index')
 def index():
 	page = request.args.get('page', 1, type=int)
-	post_query = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+	post_query = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
 	return render_template('home.html', title='Home', post=post_query)
 
 
